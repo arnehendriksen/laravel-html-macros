8d8bcab6e13f66beb6b5e6188d1131270997ec73
Arne Hendriksen <arne.hendriksen@gmail.com>
2019-11-28 08:44:42 +0100

    webFontLoader

--- src/google.php
+++ src/google.php
@@ -1,4 +1,5 @@
 <?php
+
 Html::macro('googleAnalytics', function ($trackingId) {
     return <<<EOT
 <!-- Global site tag (gtag.js) - Google Analytics -->
@@ -9,6 +10,24 @@ Html::macro('googleAnalytics', function ($trackingId) {
   gtag('js', new Date());
 
   gtag('config', '{$trackingId}');
+</script>
+EOT;
+});
+
+Html::macro('webFontLoader', function ($families) {
+    return <<<EOT
+<script type="text/javascript">
+    WebFontConfig = {
+        google: { families: [ {$families} ] }
+    };
+    (function() {
+        var wf = document.createElement('script');
+        wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+        wf.type = 'text/javascript';
+        wf.async = 'true';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(wf, s);
+    })();
 </script>
 EOT;
 });
