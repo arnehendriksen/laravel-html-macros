6d211ab90a47674802373a9826b22331bd790db9
Arne Hendriksen <arne.hendriksen@gmail.com>
2019-11-28 09:37:19 +0100

    open graph

--- src/open-graph.php
+++ src/open-graph.php
@@ -1,6 +1,6 @@
 <?php
 
-Html::macro('ogBasic', function (array $data) {
+Html::macro('openGraph', function (array $data) {
     $html = '';
     foreach ($data as $property => $content) {
         $html .= '<meta property="og:'.$property.'" content="'.$content.'" />'."\n";
