e65e602d0502b3897062ba329b54a4f4e5862bda
Arne Hendriksen <arne.hendriksen@gmail.com>
2019-11-28 11:03:03 +0100

    open graph + twitter card macros

--- src/open-graph.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-Html::macro('openGraph', function (array $data) {
-    $html = '';
-    foreach ($data as $property => $content) {
-        $html .= '<meta property="og:'.$property.'" content="'.$content.'" />'."\n";
-    }
-    return $html;
-});
---
+++ src/social.php
@@ -0,0 +1,18 @@
+<?php
+
+// Note: add  prefix="og: http://ogp.me/ns#" to your <html> tag!
+Html::macro('openGraph', function (array $data) {
+    $html = '';
+    foreach ($data as $property => $content) {
+        $html .= '<meta property="og:'.$property.'" content="'.$content.'" />'."\n";
+    }
+    return $html;
+});
+
+Html::macro('twitterCard', function (array $data) {
+    $html = '';
+    foreach ($data as $name => $content) {
+        $html .= '<meta name="twitter:'.$name.'" content="'.$content.'" />'."\n";
+    }
+    return $html;
+});
