78da2076c86c367a7b37bb4fef596b2df67f0c36
Arne Hendriksen <arne.hendriksen@gmail.com>
2019-12-21 07:40:20 +0100

    fix

--- src/google.php
+++ src/google.php
@@ -21,7 +21,7 @@ Html::macro('gtm', function (string $containerId) {
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','{$containerId}}');</script>
+})(window,document,'script','dataLayer','{$containerId}');</script>
 <!-- End Google Tag Manager -->
 EOT;
 });
