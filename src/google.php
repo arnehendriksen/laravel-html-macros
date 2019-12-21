9bac98907bdb6ec25d97c54799d2881860e78833
Arne Hendriksen <arne.hendriksen@gmail.com>
2019-12-21 07:04:38 +0100

    Google Tag Manager macros

--- src/google.php
+++ src/google.php
@@ -14,6 +14,27 @@ Html::macro('googleAnalytics', function (string $trackingId) {
 EOT;
 });
 
+Html::macro('gtm', function (string $containerId) {
+    return <<<EOT
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','{$containerId}}');</script>
+<!-- End Google Tag Manager -->
+EOT;
+});
+
+Html::macro('gtmNoScript', function (string $containerId) {
+    return <<<EOT
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id={$containerId}"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+EOT;
+});
+
 Html::macro('webFontLoader', function (array $families) {
     $families = implode('\', \'', $families);
     return <<<EOT
